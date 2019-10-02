f69d9303a3143312533e07d966462f213f13963b
catherine-o <catherine.ohara@outlook.com>
2019-10-02 15:59:43 -0600

    adds ParkDetails buttons, opens link

--- src/components/cards/ParkDetails.js
+++ src/components/cards/ParkDetails.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
 export default function ParkDetails({park}) {
+
+    const openLink = () => {
+        window.open(park.url, "_blank")
+    }
+
     return (
         <div>
             <p>{park.description}</p>
             <h4>Climate:</h4>
             <p>{park.weatherInfo}</p>
+            <button onClick={openLink}>➤</button>
+            <button>Back</button>
+            <button>♡</button>
         </div>
     )
 }
